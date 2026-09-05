b6959470bd76339b48221aeb1eb1ee48a5d8c6f1
Swarrrrovski <thrillophile678@gmail.com>
2026-09-05 20:06:07 +0530

    Update Gamecontrols.cs
    initializing variables

--- Assets/SCRIPTS/Gamecontrols.cs
+++ Assets/SCRIPTS/Gamecontrols.cs
@@ -1,18 +1,68 @@
 using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
-public class Gamecontrols : MonoBehaviour
+/// <summary>
+/// Main controller for the slot machine.
+///
+/// GameControl manages:
+/// - Betting
+/// - The bet popup
+/// - The slot-machine handle
+/// - Sequential reel spinning
+/// - Winning combination detection
+/// - Payout calculation
+public class GameControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Reels")]
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // Three reels in left-to-right order.
+    [SerializeField]
+    private Reels[] rows;
+
+
+    [Header("Handle")]
+
+    // Animator attached to the slot handle.
+    [SerializeField]
+    private Animator handleAnimator;
+
+
+    // Name of the Animator Trigger used to pull the lever.
+    [SerializeField]
+    private string pullTriggerName = "Pull";
+
+
+    [Header("Bet Popup")]
+
+    // Your Bet 10G / 50G / 100G popup.
+    [SerializeField]
+    private GameObject betPopup;
+
+
+    [Header("UI")]
+
+    [SerializeField]
+    private TMP_Text resultText;
+
+
+    [SerializeField]
+    private TMP_Text coinText;
+
+
+    [SerializeField]
+    private TMP_Text selectedBetText;
+
+
+    [Header("Player Coins")]
+
+    [SerializeField]
+    private int startingCoins = 1000;
+
+
+    private int coinBalance;
+
+    private int selectedBet = 0;
+
+    private bool isSpinning = false;
 }
